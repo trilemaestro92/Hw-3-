4dd8d5ea394a229740d1ae3343e97d095997ef90
Tri <trihuynguyen14@gmail.com>
2018-09-01 15:28:03 -0400

    add header scroll

---
+++ app.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementsByClassName("head");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    
+  } else {
+    header.classList.remove("sticky");
+    
+  }
+}
